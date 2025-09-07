cc39db5bcad072e9cd8cacfe37b4149468eb0e35
aml-abdelrhman <Aml586651@gmail.com>
2025-09-08 02:58:49 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import MyNavbar from './components/MyNavbar';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import MyNavbar from './Components/MyNavbar';
 import Hero from './Components/Hero';
-import Order from './components/Order';
-import About from './components/About';
-import Menu from './components/Menu';
-import Reservation from './components/Reservation';
-import Events from './components/Events';
-import Gallery from './components/Gallery';
-import Testimonials from './components/Testimonials';
-import Contact from './components/Contact';
-import MyFooter from './components/MyFooter';
-import EventDetails from './components/EventDetails';
+import Order from './Components/Order';
+import About from './Components/About';
+import Menu from './Components/Menu';
+import Reservation from './Components/Reservation';
+import Events from './Components/Events';
+import Gallery from './Components/Gallery';
+import Testimonials from './Components/Testimonials';
+import Contact from './Components/Contact';
+import MyFooter from './Components/MyFooter';
+import EventDetails from './Components/EventDetails';
 
 function App() {
   return (
